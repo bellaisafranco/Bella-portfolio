cdd494668992355bc6ffb59daeb37e4e8e18e0e8
bellaisafranco <isabella.franco@edu.sait.ca>
2024-12-12 01:48:07 -0700

    im going to sleep now i tried

--- scripts.js
+++
@@ -1,8 +0,0 @@
-const words = ["fruity","spring","juice","bitter","ripe","seasonal","messy","sweet","sour","delicious"];
-const yumButton = document.getElementById("yumButton");
-function generateRandomWord(){
-    const randomIndex = Math.floor(Math.random()*words.length);
-    const randomWord = words[randomIndex];
-    document.querySelector("p").textContent='Random Word: ${randomWord}';
-}
-yumButton.addEventListener("click", generateRandomWord);
