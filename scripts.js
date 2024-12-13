ae883cab195ca82fe5e393073a997927e33e2b54
bellaisafranco <isabella.franco@edu.sait.ca>
2024-12-12 23:49:06 -0700

    im sorry

---
+++ scripts.js
@@ -0,0 +1,28 @@
+fit1.addEventListener('click',function(){
+
+    console.log("im crying");
+    document.getElementById('gfit').src="brownblack.png";
+    document.body.classList.remove("gfit");
+    document.body.classList.add("brownblack.png");
+})
+fit2.addEventListener('click',function(){
+    
+    console.log("im crying");
+    document.getElementById('gfit').src="brownbrownblack.png";
+    document.body.classList.remove("gfit");
+    document.body.classList.add("brownbrownblack.png");
+})
+fit3.addEventListener('click',function(){
+
+    console.log("im crying");
+    document.getElementById('gfit').src="redfit.png";
+    document.body.classList.remove("gfit");
+    document.body.classList.add("redfit.png");
+})
+fit4.addEventListener('click',function(){
+
+    console.log("im crying");
+    document.getElementById('gfit').src="redbrown.png";
+    document.body.classList.remove("gfit");
+    document.body.classList.add("redbrown.png");
+})
